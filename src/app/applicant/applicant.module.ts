6f4ef64b168ddd14ccb05cbaf4d2bb4fed214cf4
Daniel Melgarejo <dhmelgarejo@gmail.com>
2017-08-14 21:45:28 -0500

    Arreglo modal registro

--- src/app/applicant/applicant.module.ts
+++ src/app/applicant/applicant.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterializeModule
+    MaterializeModule,
+    FormsModule 
   ],
   declarations: [RegisterComponent],
   exports: [
